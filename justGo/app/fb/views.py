8297c4656d8f054c363a8cc2ab792014db8f7976
jinjoo choi <jinjoooooooo@gmail.com>
2017-06-06 04:02:00 +0000

    Fix fb chat flow

--- justGo/app/fb/views.py
+++ justGo/app/fb/views.py
@@ -3,6 +3,7 @@ from flask import Flask, request, Blueprint
 from .. import PathManager,NLCManager, NLPManager
 from ..config.config import Config
 from . import fb_messenger, FBMessengerManager
+from ..models.NLPResult import NLPResultCode
 
 
 @fb_messenger.route('', methods=['GET', 'POST'])
@@ -31,12 +32,19 @@ def webhook():
            elif x['message'].get('text'):
              nlc_result = NLCManager.analysis(x['message']['text'])
              if nlc_result == Config.NLC_CLASS_GREETING:
-               FBMessengerManager.sendTextMessage(recipient_id,'안녕 ㅎㅎ')
+               FBMessengerManager.sendTextMessage(recipient_id,'안녕하세요.ㅎㅎ')
+             elif nlc_result == Config.NLC_CLASS_ASK_NAME:
+               FBMessengerManager.sendTextMessage(recipient_id, '제 이름은 저스트고입니다.ㅎㅎ')
+             elif nlc_result == Config.NLC_CLASS_SLANG:
+               FBMessengerManager.sendTextMessage(recipient_id, '그러지말구 대중교통에 대해서 물어봐줄래요? :)')
              elif nlc_result == Config.NLC_CLASS_SEARCH_PATH:
                nlp_result = NLPManager.findSrcAndDest(x['message']['text'])
                print(nlp_result)
-               result = PathManager.search(nlp_result)
-               FBMessengerManager.sendReplyMessage(recipient_id, result)            
+               if nlp_result.result_code == NLPResultCode.UNSUPPORTED_FORMAT:
+                 FBMessengerManager.sendTextMessage(recipient_id,nlp_result.getErrorMessage())
+               else:
+                 result = PathManager.search(nlp_result.src, nlp_result.dest)
+                 FBMessengerManager.sendReplyMessage(recipient_id, result)            
          else:
            pass
   return "Success"
